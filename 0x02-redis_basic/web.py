cd05169a56c4885b99f724635402521d53851c31
Gold Israel <techiegold101@gmail.com>
2024-04-01 15:23:25 +0100

    fix code

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,23 +4,28 @@
 import requests
 import redis
 from functools import wraps
-from typing import Callable
 
-redis = redis.Redis()
+store = redis.Redis()
 
 
-def count_url_access(fn: callable) -> callable:
+def count_url_access(method):
     """Decorator counting how many times a URL is accessed"""
-    @wraps(fn)
+    @wraps(method)
     def wrapper(url):
-        redis.incr(f"count:{url}")
-        cached_resp = redis.get(f"cached:{url}")
-        if cached_resp:
-            return (cached_resp.decode('utf-8'))
-        result = fn(url)
-        redis.setex(f"cached:{url}", 10, result)
-        return (result)
-    return (wrapper)
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
+        html = method(url)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
+    return wrapper
+
 
 
 @count_url_access
