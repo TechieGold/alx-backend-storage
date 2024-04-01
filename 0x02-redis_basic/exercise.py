2bf50338bd0c948ba1621db55c3e29d8f562871d
Gold Israel <techiegold101@gmail.com>
2024-04-01 06:45:21 +0100

    add three extra methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,12 +5,13 @@ stores an instance of Redis client as a private varibale and
 a store method that takes a data argument and returns a string
 """
 import redis
-from typing import Union
+from typing import Union, Callable
 import uuid
 
 
 class Cache:
     """ Cache class with two methods (__init__ and store)"""
+
     def __init__(self):
         """Stores and flush an instance of redis."""
         self._redis = redis.Redis()
@@ -21,3 +22,22 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return (key)
+
+    def get(self, key: str, fn: Callable = None) \
+            -> Union[str, bytes, int, None]:
+        """ Retrieves and converts a value to the required format,
+            before returning it."""
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fn(value)
+        return (value)
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """Retrives value, decode it as UTF-8 and return a string."""
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """ Retrives value, converts to an integer before returining it."""
+        return self.get(key, fn=int)
